cf89cdf9c44e40ed7c566bd15194daef96c16a29
Mofesola Banjo <mofesola@ito.lt>
2019-12-04 13:56:23 +0200

    Completed Bitwise class

--- src/Bitwise.php
+++ src/Bitwise.php
@@ -28,6 +28,7 @@ class Bitwise
     private function __construct(string $value)
     {
         $this->value = $value;
+        $this->formatValue($value);
     }
 
     /**
@@ -47,10 +48,46 @@ class Bitwise
      */
     public static function createFromString(string $string): Bitwise
     {
-        if (preg_match("/[^01]/", $string)) {
+        if (!self::isBinary($string)) {
             throw new Exception("Invalid binary number supplied");
         }
         return new self(strval($string));
+    }
+
+    /**
+     * @param string $string
+     * @return bool
+     */
+    public static function isBinary(string $string): bool
+    {
+        return !preg_match("/[^01]/", $string);
+    }
+
+    /**
+     * @param string|null $incoming
+     * @return int
+     */
+    private function minOutputLength(?string $incoming = ''): int
+    {
+        return max(
+            strlen($this->value),
+            strlen($incoming)
+        );
+    }
+
+    /**
+     * @param int|string $newValue
+     * @param int $minLength
+     */
+    private function formatValue($newValue, int $minLength = 3): void
+    {
+        $valueInBinary = is_int($newValue) ? decbin($newValue) : $newValue;
+        $newLength = strlen($valueInBinary);
+        if ($newLength < $minLength) {
+            $zeroes = str_repeat(0, $minLength - $newLength);
+            $valueInBinary =  "{$zeroes}$valueInBinary";
+        }
+        $this->value = $valueInBinary;
     }
 
     /**
@@ -67,5 +104,45 @@ class Bitwise
     public final function getValueAsInteger(): int
     {
         return bindec($this->value);
+    }
+
+    /**
+     * @param int|Bitwise $input
+     * @param bool $updateValue
+     */
+    public final function and($input, $updateValue = true): int
+    {
+        $value = $input instanceof Bitwise ? $input->getValueAsInteger() : $input;
+        $result = $this->getValueAsInteger() & $value;
+        if ($updateValue) {
+            $this->formatValue($result, $this->minOutputLength($result));
+        }
+        return $result;
+    }
+
+    /**
+     * @param int|Bitwise $input
+     */
+    public final function or($input, $updateValue = true): int
+    {
+        $value = $input instanceof Bitwise ? $input->getValueAsInteger() : $input;
+        $result = $this->getValueAsInteger() | $value;
+        if ($updateValue) {
+            $this->formatValue($result, $this->minOutputLength($result));
+        }
+        return $result;
+    }
+
+    /**
+     * @param int|Bitwise $input
+     */
+    public final function xor($input, $updateValue = true): int
+    {
+        $value = $input instanceof Bitwise ? $input->getValueAsInteger() : $input;
+        $result = $this->getValueAsInteger() ^ $value;
+        if ($updateValue) {
+            $this->formatValue($result, $this->minOutputLength($result));
+        }
+        return $result;
     }
 }
--- tests/BitwiseTest.php
+++ tests/BitwiseTest.php
@@ -21,6 +21,12 @@ class BitwiseTest extends TestCase
     }
 
     /** @test */
+    public final function bitwise_object_formats_binary_output(): void {
+        $bitwiseObject = Bitwise::createFromInteger(1);
+        self::assertEquals("001", $bitwiseObject->getValue());
+    }
+
+    /** @test */
     public final function bitwise_object_can_be_created_with_string_value(): void
     {
         $bitwiseObject = Bitwise::createFromString("11");
@@ -35,6 +41,64 @@ class BitwiseTest extends TestCase
     public final function error_is_thrown_when_non_binary_string_is_used_for_object_creation(): void
     {
         self::expectExceptionMessage("Invalid binary number supplied");
-        $bitwise = Bitwise::createFromString("mofesola");
+        $bitwise = Bitwise::createFromString("101m");
+    }
+
+    /** @test */
+    public final function bitwise_class_can_identify_binary_number(): void
+    {
+        self::assertTrue(Bitwise::isBinary("101"));
+        self::assertFalse(Bitwise::isBinary("101m"));
+    }
+
+    /** @test */
+    public final function bitwise_AND_works_with_integer_input(): void
+    {
+        $bitwise = Bitwise::createFromInteger(5);
+        $bitwise->and(3);
+        self::assertEquals("001", $bitwise->getValue());
+    }
+
+    /** @test */
+    public final function bitwise_AND_works_with_bitwise_input(): void
+    {
+        $bitwiseA = Bitwise::createFromInteger(5);
+        $bitwiseB = Bitwise::createFromInteger(3);
+        $bitwiseA->and($bitwiseB);
+        self::assertEquals("001", $bitwiseA->getValue());
+    }
+
+    /** @test */
+    public final function bitwise_OR_works_with_integer_input(): void
+    {
+        $bitwise = Bitwise::createFromInteger(5);
+        $bitwise->or(3);
+        self::assertEquals("111", $bitwise->getValue());
+    }
+
+    /** @test */
+    public final function bitwise_OR_works_with_bitwise_input(): void
+    {
+        $bitwiseA = Bitwise::createFromInteger(5);
+        $bitwiseB = Bitwise::createFromInteger(3);
+        $bitwiseA->or($bitwiseB);
+        self::assertEquals("111", $bitwiseA->getValue());
+    }
+
+    /** @test */
+    public final function bitwise_XOR_works_with_integer_input(): void
+    {
+        $bitwise = Bitwise::createFromInteger(5);
+        $bitwise->xor(3);
+        self::assertEquals("110", $bitwise->getValue());
+    }
+
+    /** @test */
+    public final function bitwise_XOR_works_with_bitwise_input(): void
+    {
+        $bitwiseA = Bitwise::createFromInteger(5);
+        $bitwiseB = Bitwise::createFromInteger(3);
+        $bitwiseA->xor($bitwiseB);
+        self::assertEquals("110", $bitwiseA->getValue());
     }
 }
